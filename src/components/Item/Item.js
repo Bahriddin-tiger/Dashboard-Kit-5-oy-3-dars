febe8173a6d3e4b4b021f4fdf9467fffb85c1a07
Bahriddin-tiger <934045436b@gmail.com>
2022-08-02 18:46:59 +0500

    Initiall commit

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,9 +1,9 @@
-import "./item.css"
-export const Item = ({img,text}) =>{
-return(
-    <li className="item">
-    <img className="item-img" src={img}/>
-    <p className="item-text">{text}</p>
-    </li>
-)
-}
+import './item.css';
+export const Item = ({ img, text }) => {
+	return (
+		<li className='item'>
+			<img className='item-img' alt=' img' src={img} />
+			<p className='item-text'>{text}</p>
+		</li>
+	);
+};
